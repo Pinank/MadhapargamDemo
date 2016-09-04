a70cb890776d99e62dbd1a16ad727c2b29b4a691
Pinank Lakhani <lakhanipinank@gmail.com>
2016-09-04 17:06:28 +0530

    All Models Complete. Serializer Work started

--- MadhaparDemo/MadhaparDemo/settings.py
+++ MadhaparDemo/MadhaparDemo/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'gamuser',
     'Location',
     'NewsFeed',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'Projects',
     'DeviceInfo',
     'OTPInfo',
+    'snippets',
 ]
 
 MIDDLEWARE = [
